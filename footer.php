710471783df82a369cc055308b489893d5082738
dsyko <dsykora@gmail.com>
2012-05-04 00:16:31 +0300

    Changed footer designer credit.
    Signed-off-by: dsyko <dsykora@gmail.com>

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 			<?php do_action( 'portforwardpodcast_credits' ); ?>
 			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'portforwardpodcast' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'portforwardpodcast' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'portforwardpodcast' ), 'portforwardpodcast', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'portforwardpodcast' ), 'portforwardpodcast', '<a href="http://Dsyko.com/" rel="designer">Dsyko</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- .site-footer .site-footer -->
 </div><!-- #page .hfeed .site -->

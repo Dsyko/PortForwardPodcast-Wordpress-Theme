1cebce3e36a58ad519fb48fab4cd1924dedab652
dsyko <dsykora@gmail.com>
2012-05-10 22:16:24 +0300

    Changing single page layout to match theme.
    Signed-off-by: dsyko <dsykora@gmail.com>

--- content-single.php
+++ content-single.php
@@ -5,55 +5,44 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-
-		<div class="entry-meta">
+<article id="post-<?php the_ID(); ?>" <?php post_class("span9"); ?>>
+	<header class="entry-header row">
+		<div class="entry-meta span1 sprite calendar">
 			<?php portforwardpodcast_posted_on(); ?>
 		</div><!-- .entry-meta -->
+		<h2 class="entry-title span7"><?php the_title(); ?></h2>
+		<div class="admin-controls span1">
+			<?php edit_post_link( __( 'Edit', 'portforwardpodcast' ), '<span class="edit-link">', '</span>' ); ?>
+		<div>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'portforwardpodcast' ), 'after' => '</div>' ) ); ?>
+	<div class="entry-content row">
+		<div class="span9">
+			<?php the_content(); ?>
+			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'portforwardpodcast' ), 'after' => '</div>' ) ); ?>
+		</div>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-meta">
-		<?php
-			/* translators: used between list items, there is a space after the comma */
-			$category_list = get_the_category_list( __( ', ', 'portforwardpodcast' ) );
-
-			/* translators: used between list items, there is a space after the comma */
-			$tag_list = get_the_tag_list( '', ', ' );
-
-			if ( ! portforwardpodcast_categorized_blog() ) {
-				// This blog only has 1 category so we just need to worry about tags in the meta text
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'portforwardpodcast' );
-				} else {
-					$meta_text = __( 'Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'portforwardpodcast' );
-				}
-
-			} else {
-				// But this blog has loads of categories so we should probably display them here
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'portforwardpodcast' );
-				} else {
-					$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'portforwardpodcast' );
-				}
-
-			} // end check for categories on this blog
-
-			printf(
-				$meta_text,
-				$category_list,
-				$tag_list,
-				get_permalink(),
-				the_title_attribute( 'echo=0' )
-			);
-		?>
-
-		<?php edit_post_link( __( 'Edit', 'portforwardpodcast' ), '<span class="edit-link">', '</span>' ); ?>
+	<footer class="entry-meta row">
+		
+			
+		<div class="span1">
+			<?php if ( !post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+			<span class="comments-link sprite numcomments"><span class="commentcountertext"><?php echo get_comments_number(); ?></span></span>
+			<?php endif; ?>
+		</div>
+		<div class="span8">
+			<?php
+				/* translators: used between list items, there is a space after the comma */
+				$tags_list = get_the_tag_list( '', __( ', ', 'portforwardpodcast' ) );
+				if ( $tags_list ) :
+			?>
+			<span class="sprite tags"></span>
+			<span class="tag-links">
+				<?php printf( __( ' %1$s', 'portforwardpodcast' ), $tags_list ); ?>
+			</span>
+			<?php endif; // End if $tags_list ?>
+		</div>
+		
 	</footer><!-- .entry-meta -->
 </article><!-- #post-<?php the_ID(); ?> -->

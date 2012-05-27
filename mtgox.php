30e81f293e8881acfee51d6a937d8b43acc69038
dsyko <dsykora@gmail.com>
2012-05-28 00:15:47 +0300

    Added a php file to get latest bitcoin rate from mtGox
    Signed-off-by: dsyko <dsykora@gmail.com>

---
+++ mtgox.php
@@ -0,0 +1,45 @@
+<?php
+function mtgox_query($path, array $req = array()) {
+		// API settings
+		$key = '';
+		$secret = '';
+	 
+		// generate a nonce as microtime, with as-string handling to avoid problems with 32bits systems
+		$mt = explode(' ', microtime());
+		$req['nonce'] = $mt[1].substr($mt[0], 2, 6);
+	 
+		// generate the POST data string
+		$post_data = http_build_query($req, '', '&');
+	 
+		// generate the extra headers
+		$headers = array(
+			'Rest-Key: '.$key,
+			'Rest-Sign: '.base64_encode(hash_hmac('sha512', $post_data, base64_decode($secret), true)),
+		);
+	 
+		// our curl handle (initialize if required)
+		static $ch = null;
+		if (is_null($ch)) {
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MtGox PHP client; '.php_uname('s').'; PHP/'.phpversion().')');
+		}
+		curl_setopt($ch, CURLOPT_URL, 'https://mtgox.com/api/'.$path);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+	 
+		// run the query
+		$res = curl_exec($ch);
+		if ($res === false) return false;
+		$dec = json_decode($res, true);
+		if (!$dec) return false;
+		return $dec;
+	}
+	
+	
+        $bitcoinRate = mtgox_query('1/BTCUSD/public/ticker');
+        
+		if($bitcoinRate === false)
+			$bitcoinRate["return"]["avg"]["display"] = "API not working...";
+		echo json_encode($bitcoinRate["return"]["avg"]["display"]);

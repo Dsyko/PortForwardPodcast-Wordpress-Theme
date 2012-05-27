8f10f07965596829da5743c4756812024b221d86
dsyko <dsykora@gmail.com>
2012-05-28 00:01:23 +0300

    Added include of tagscanvis jquery plugin for donors sidebar
    Signed-off-by: dsyko <dsykora@gmail.com>

--- header.php
+++ header.php
@@ -43,6 +43,7 @@
 
 <?php wp_head(); ?>
 <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.timeago.js" type="text/javascript"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.tagcanvas.min.js" type="text/javascript"></script>
 </head>
 
 <body <?php body_class(); ?>>
